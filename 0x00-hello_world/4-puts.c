89bc18a097270b39808555b56bda65f0a28a97c4
Somie1 <somelezesipayile@gmail.com>
2022-10-28 15:24:14 -0700

    number 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
-
 /**
- * main - Entry point
+ * main - Entry
  *
- * Return: Always 0 (success)
+ * Return: Always 0
  */
 int main(void)
 {
 	puts("\"Programming is like building multilingual puzzle");
 	return (0);
-
 }
